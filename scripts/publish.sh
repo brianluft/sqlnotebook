59f74f0f9dd037c60d1219b5c24feb164e5a0306
Brian Luft <brian@electroly.com>
2025-06-08 05:21:07 -0400

    Add publish.sh

---
+++ scripts/publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+cd ..
+PLATFORM=$(scripts/get-native-arch.sh)
+MSBUILD_PLATFORM="${PLATFORM,,}"
+
+# Use vswhere to locate msbuild.exe
+VSWHERE="/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe"
+if [ ! -f "$VSWHERE" ]; then
+    echo "Could not find vswhere.exe!"
+    exit 1
+fi
+
+MSBUILD=$("$VSWHERE" -latest -requires Microsoft.Component.MSBuild -find "MSBuild/**/Bin/MSBuild.exe" | head -n 1)
+if [ ! -f "$MSBUILD" ]; then
+    echo "Could not find msbuild.exe!"
+    exit 1
+fi
+
+powershell.exe -NoProfile ps1/New-Release.ps1 -Platform "$MSBUILD_PLATFORM" -MsbuildPath "'$MSBUILD'"
